634e51ae2703dd58a3d159ab61c4f36b8493d5db
Dahamq <induwaradaham99@gmail.com>
2024-07-08 23:57:20 +0530

    Navigation bare Style Added styles to Navigation bar using CSS.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navigation from './Navigation'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navigation from "./Navigation";
 
 function App() {
-
-
   return (
-    
-      <main>
-       <Navigation/>
-      </main>        
-   
-  )
+    <main>
+      <Navigation />
+    </main>
+  );
 }
 
-export default App
+export default App;
