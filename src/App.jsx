d375188c31ccef7a5db086aff3d3251cff2a32af
Tilshan Ranasinghe <2013pamaljith@gmail.com>
2024-12-24 21:34:44 +0530

    Removed unused comments.

--- src/App.jsx
+++ src/App.jsx
@@ -1,118 +1,7 @@
-
-// import { BrowserRouter, Routes, Route, Navigate, } from "react-router-dom";
-// import Navigation from "./Navigation";
-// import Hero from "./Hero";
-// import Footer from "./Footer";
-// import AboutUs from "./AboutUs";
-// import ContactUs from "./ContactUs";
-// import Register from "./login-register components/Register"
-
-// import Login from "./login-register components/Login"
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       {/* Navigation is common to all pages */}
-//       <Navigation />
-      
-      
-//       {/* Routes for page-specific content */}
-//       <Routes>
-//         {/* Home Page */}
-//         <Route path="/" element={<Hero />} />
-        
-//         {/* About Us Page */}
-//         <Route path="/aboutUs" element={<AboutUs />} />
-        
-//         {/* Contact Us Page */}
-//         <Route path="/contactUs" element={<ContactUs />} />
-
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-        
-//         {/* Fallback Route */}
-//         <Route path="*" element={<Navigate to="/" />} />
-//       </Routes>
-      
-//       {/* Footer is common to all pages */}
-//       <Footer />
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-// import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
-// import Navigation from "./Navigation";
-// import Hero from "./Hero";
-// import Footer from "./Footer";
-// import AboutUs from "./AboutUs";
-// import ContactUs from "./ContactUs";
-// import Register from "./login-register components/Register";
-// import Login from "./login-register components/Login";
-
-// function App() {
-//   const location = useLocation();
-//   const noNavbarFooter = location.pathname === "/login" || location.pathname === "/register";
-
-//   return (
-//     <BrowserRouter>
-//       {!noNavbarFooter && <Navigation />}
-
-//       {/* Routes for page-specific content */}
-//       <Routes>
-//         {/* Home Page */}
-//         <Route path="/" element={<Hero />} />
-
-//         {/* About Us Page */}
-//         <Route path="/aboutUs" element={<AboutUs />} />
-
-//         {/* Contact Us Page */}
-//         <Route path="/contactUs" element={<ContactUs />} />
-
-//         {/* Login Page */}
-//         <Route path="/login" element={<Login />} />
-
-//         {/* Register Page */}
-//         <Route path="/register" element={<Register />} />
-
-//         {/* Fallback Route */}
-//         <Route path="*" element={<Navigate to="/" />} />
-//       </Routes>
-
-//       {!noNavbarFooter && <Footer />}
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Navigation from "./Navigation";
 import Hero from "./Hero";
 import Footer from "./Footer";
-import AboutUs from "./AboutUs";
-import ContactUs from "./ContactUs";
 import Register from "./login-register components/Register";
 import Login from "./login-register components/Login";
 
@@ -128,12 +17,6 @@ function AppContent() {
       <Routes>
         {/* Home Page */}
         <Route path="/" element={<Hero />} />
-
-        {/* About Us Page */}
-        <Route path="/aboutUs" element={<AboutUs />} />
-
-        {/* Contact Us Page */}
-        <Route path="/contactUs" element={<ContactUs />} />
 
         {/* Login Page */}
         <Route path="/login" element={<Login />} />
